5601a370f3c2b0246ba23c326bd840f352b714ff
nakamoto <tomoya_nakamoto@safarigames.com>
2024-04-18 10:46:48 +0900

    リストとマウスカーソルを非表示にする

---
+++ src/StarterPlayer/StarterPlayerScripts/Invisible.client.lua
@@ -0,0 +1,8 @@
+--プレイヤーリストの非表示
+local GameStartGui = game:GetService("StarterGui")
+GameStartGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList,false)
+
+--マウスカーソルの非表示
+local UserInputService = game:GetService("UserInputService")
+UserInputService.MouseIconEnabled = false
+
