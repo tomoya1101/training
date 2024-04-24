8ace99eef405ef7312c70b0cdd6c28b6e8a810d6
nakamoto <tomoya_nakamoto@safarigames.com>
2024-04-24 18:33:49 +0900

    Playerジャンプできないようにした

--- src/StarterPlayer/StarterPlayerScripts/Invisible.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/Invisible.client.lua
@@ -9,3 +9,16 @@ local UserInputService = game:GetService("UserInputService")
 --一人称視点に変更
 local player = game.Players.LocalPlayer
 --player.CameraMode = Enum.CameraMode.LockFirstPerson
+
+
+--プレイヤーについているHumanoidを取得
+local Players = game:GetService("Players")
+local Player = Players:FindFirstChildOfClass("Player")
+while task.wait() do
+    --nilチェック プレイヤー存在しているか
+	if Player.Character then
+        local Humanoid = Player.Character:FindFirstChildOfClass("Humanoid")	
+        --プレイヤーをジャンプできないようにする
+        Humanoid:SetStateEnabled(Enum.HumanoidStateType.Jumping,false) 
+    end
+end
